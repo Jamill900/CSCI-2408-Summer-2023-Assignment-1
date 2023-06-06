cb5e89e622fc47c4ed3becbb009be04a8fdabda4
Jamil Alizada <xalizadax@gmail.com>
2023-06-06 12:53:46 +0400

    Task 1

--- 01_triangle/app.js
+++ 01_triangle/app.js
@@ -1,6 +1,5 @@
 onload = () => {
     let canvas = document.getElementById('webgl-canvas');
-
     let gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { 
         alert("Couldn't setup webgl"); 
@@ -12,6 +11,35 @@ onload = () => {
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
+
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let vertices = [
+        -.5, -.5,
+         0,  .5,
+         .5, -.5
+         
+    ];
+
+
+    let vBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    let vPosition = gl.getAttribLocation(program, 'vPosition');
+
+    gl.vertexAttribPointer(
+        vPosition,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    )
+
+    gl.enableVertexAttribArray(vPosition);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
